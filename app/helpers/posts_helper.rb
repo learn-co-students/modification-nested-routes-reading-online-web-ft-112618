900d01b434276f54ae3561ebb9dd3e2474192461
Andrew Johnson <ajohnson.uva@gmail.com>
2019-02-18 11:01:05 -0500

    Done.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,3 +1,11 @@
 module PostsHelper
-  
+
+  def author_id_field(form_builder)
+    unless form_builder.object.author
+      form_builder.collection_select :author_id, Author.all, :id, :name
+    else
+      form_builder.hidden_field :author_id
+    end
+  end
+
 end
